b52110585c2ec713bc5e347e6635cb46425e149b
Mixowo <eduardoperezpro@gmail.com>
2024-09-15 11:49:35 -0700

    y tu?

--- CapaDatos/CD_Usuario.cs
+++ CapaDatos/CD_Usuario.cs
@@ -19,7 +19,7 @@ namespace CapaDatos
             using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
             {
                 //hola jorge como estass
-                //bien hola
+                //bien hola y tu?
                 try
                 {
                     string query = "select IdUsuario,Documento,NombreCompleto,Correo,Clave,Estado from usuario";
